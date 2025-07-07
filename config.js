51be66d82766e45f0267334b5a1125302f1c13d0
tumaweb12 <kimanthileone@gmail.com>
2025-07-07 15:14:29 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+// config.js
+window.TUMA_CONFIG = {
+  SUPABASE_URL: 'https://your-supabase-project.supabase.co',
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJI…',         // safe to expose
+  OSMR_ENDPOINT: 'https://router.project-osrm.org'
+}
