294a158a7d0f177d07fc717c7996d8b2c6876cc5
tumaweb12 <kimanthileone@gmail.com>
2025-08-26 19:26:54 +0300

    Update route-optimizer.js

--- route-optimizer.js
+++ route-optimizer.js
@@ -1417,8 +1417,6 @@ class RouteOptimizer {
     }
 }
 
-// Export for use in other modules
-export default RouteOptimizer;
 // Make available globally for non-module environments
 if (typeof window !== 'undefined') {
     window.RouteOptimizer = RouteOptimizer;
